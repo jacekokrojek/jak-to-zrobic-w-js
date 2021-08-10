b413ae6f7fe7474eefd9cceedd835ee23bda18d8
Krystian Plich <KrystianPlich@github.com>
2021-08-10 15:02:08 +0200

    Task1 completed

--- excercsises/1_introduction/minimal.spec.js
+++ excercsises/1_introduction/minimal.spec.js
@@ -15,18 +15,21 @@ describe('Protractor workshop app', function() {
 		var expectedHTML = "Copyright © 2013 Shapebootstrap | All Rights Reserved"
 		expect(footerCopyright.getText()).toContain(expectedHTML)
 	});
-
 	/**
 	 * Check http://angular.github.io/protractor/#/api?view=ElementFinder
 	 * to see how to select element for verification
 	 */
 
-	xit('should have "Example headline 1" carousel item after entering site', function(){	
-
+	it('should have "Example headline 1" carousel item after entering site', function(){	
+		var firstActiveItemTagText = element(by.className("active item")).element(by.className("carousel-caption")).element(by.tagName('h1'));
+		var expectedHeadline = "Example Headline 1";
+		expect(firstActiveItemTagText.getText()).toContain(expectedHeadline);
 	});
 	
-	xit('should have correct feature header', function(){
-
+	it('should have correct feature header', function(){
+		var firstActiveItemTagText = element(by.className("span12 cnt-title")).element(by.css('h1'));
+		var expectedElementText = "At vero eos et accusamus et iusto odio dignissimos"
+		expect(firstActiveItemTagText.getText()).toEqual(expectedElementText);
 	});
 
 	/**
@@ -34,7 +37,19 @@ describe('Protractor workshop app', function() {
 	 * to see how get function can be used
 	 */
 	it('should have menu items with links to "Home", "About", "Services", "Blog", "Contact",  pages',function(){
-		var menuItems = element.all(by.css('ul.nav > li > a'));		
+		var expectedItemsText = ["Home", "About", "Services", "Blog", "Contact"]
+		var menuItems = element.all(by.css('ul.nav > li > a'))//.then(function(items) {
+		// 	expect(items.length).toBe(5);
+		// 	items.forEach(function (item, i) {
+		// 		expect(item.getText()).toContain(expectedItemsText[i])
+		// 		expect(item.getAttribute('href'))
+		// 	})
+		// });
+		expect(menuItems.get(0).getText()).toBe(expectedItemsText[0]);
+		expect(menuItems.get(1).getText()).toBe(expectedItemsText[1]);
+		expect(menuItems.get(2).getText()).toBe(expectedItemsText[2]);
+		expect(menuItems.get(3).getText()).toBe(expectedItemsText[3]);
+		expect(menuItems.get(4).getText()).toBe(expectedItemsText[4]);
 	});
 
 	 /**
@@ -43,7 +58,18 @@ describe('Protractor workshop app', function() {
 	 */
 
 	it('should have Feature A, Feature B, Feature C sections ...', function(){
-		var features = element.all(by.xpath('//h2'));
+		var features = element.all(by.xpath('//h2')).map(function(elm, index) {
+			return {
+				index: index,
+				text: elm.getText(),
+			}
+		});
+		var expectedAtr = [
+			{index: 0, text: 'Feature A'},
+			{index: 1, text: 'Feature B'},
+			{index: 2, text: 'Feature C'}
+		]
+		expect(features).toEqual(expectedAtr);
 	});
 
 	/**
@@ -51,7 +77,14 @@ describe('Protractor workshop app', function() {
 	 * to see how fileter function can be used select elements based on condition
 	 */	
 	it('should route to "Blog" pages after selecting link',function(){
-		var menuItems = element.all(by.css('ul.nav > li > a'));
+		var menuItems = element.all(by.css('ul.nav > li > a')).filter(function(elem, index) {
+			return elem.getText().then(function(text) {
+				return text === 'Blog';
+			});
+		}).first().click();
+		var newPageHeader = element(by.className('page-header')).element(by.tagName('h1'));
+		expect(newPageHeader.getText()).toEqual('Blog');
+		//expect(browser.getCurrentUrl()).toContain('blog.html');
 	});
 
 });
